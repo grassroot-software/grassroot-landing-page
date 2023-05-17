efe8caf4ae131883d5e3460996218fc9adddcd2f
Adetayo Boluwatife Chritiana <Samuelchristiana38@gmail.com>
2023-05-17 11:50:24 +0100

    Update Criteria.jsx

--- src/pages/criteria/Criteria.jsx
+++ src/pages/criteria/Criteria.jsx
@@ -18,9 +18,12 @@ const Criteria = () => {
                 <article className='signup_option'>
                     <h4 className='signup_option_h4'>For Africans</h4>
                     <ul>
-                        <li><GiCheckMark/>  Must be of African origin</li>
-                        <li><GiCheckMark/>  Age between 18-34 years</li>
-                        <li><GiCheckMark/>  Access to a computer and internet during the programme</li>  
+                          
+                        <li><GiCheckMark/>  Must be of African origin.</li>
+                        <li><GiCheckMark/>  May not pay full tuition until they graduate and start working.</li>
+                        <li><GiCheckMark/>  Age between 18-34 years.</li>
+                        <li><GiCheckMark/>  Access to a computer and internet during the programme.</li>  
+                       
                     </ul>
                 </article>
                 <article className='signup_option'>
