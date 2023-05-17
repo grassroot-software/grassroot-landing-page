75dfa625655b4511c3527305cdbf030777244269
Adetayo Boluwatife Chritiana <Samuelchristiana38@gmail.com>
2023-05-17 11:41:24 +0100

    Update Payments.jsx

--- src/pages/payments/Payments.jsx
+++ src/pages/payments/Payments.jsx
@@ -24,16 +24,25 @@ const Payments = () => {
             </div>
             <div className='about__section-content'>
                 <h2>Payments</h2><br></br>
-                <p>The fees for the  programme must be paid in full as detailed in the table. Enrolment is not complete until Grassroot has received full payment of the fees. 
-                  Accepted applicants will receive an automatically generated invoice which includes a payment link from a robust global payment gateway.
-                  Accepted payment methods are VISA, Mastercard, or an International Wire Transfer.  
-                  The Grassroot team will send an email to the applicant acknowledging receipt of funds, followed by an email confirmation of enrolment from Grassroot team.
+                <h3>FOR AFRICANS</h3>
+                <p>Grassroot offers eligible African candidates access to world-class programme training at no cost. Each African candidate has the opportunity to apply for the full technical training at no upfront cost.  They only pay tuition  after they graduate and start working.
+
+
                 </p> <br></br>
+<h3>FOR NON-AFRICANS</h3>
+                <p>
+The fees for the programme must be paid in full as detailed in the table. Enrolment is not complete until Grassroot has received full payment of the fees. Accepted applicants will receive an automatically generated invoice which includes a payment link from a robust global payment gateway. Accepted payment methods are VISA, Mastercard, or an International Wire Transfer. The Grassroot team will send an email to the applicant acknowledging receipt of funds, followed by an email confirmation of enrolment from Grassroot team.
+
+
+
+
+                </p> <br></br>
+
+
                 <h2>Cancellation</h2><br></br>
-                <p>Notice of cancellation or rescheduling more than five (5) full business days prior to programme start date is eligible for a full refund or reschedule without penalty.
-                  Notice of cancellation/rescheduling on or after the fifth (5th) full business day prior to programme start date is not eligible for refund and is subject to approval and a reschedule penalty may apply.
-                  To request a reschedule within the qualifying period,  a future date must be identified immediately. In the event, the request is accepted the rescheduled date is limited to one time and for only the same course. The new course must begin up to a maximum of twelve months from the original course date.
-                  For no-shows, payment is non-refundable. “No Shows” is defined as the student does not show up on the date the course starts, and there was no prior communication from the student to cancel or reschedule the course.
+                <p>
+Notice of cancellation or rescheduling more than five (5) full business days prior to programme start date is eligible for a full refund or reschedule without penalty. Notice of cancellation/rescheduling on or after the fifth (5th) full business day prior to programme start date is not eligible for refund and is subject to approval and a reschedule penalty may apply. To request a reschedule within the qualifying period, a future date must be identified immediately. In the event, the request is accepted the rescheduled date is limited to one time and for only the same course. The new course must begin up to a maximum of twelve months from the original course date. For no-shows, payment is non-refundable. “No Shows” is defined as the student does not show up on the date the course starts, and there was no prior communication from the student to cancel or reschedule the course.
+
                 </p>
             </div>
             
