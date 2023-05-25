e9ae7d1048434988beb9ffe7467e4d7b3d6790f1
Yaya HC <72512911+yayahc@users.noreply.github.com>
2023-05-25 20:17:38 +0000

    Update paystack.js

--- src/payment-api/paystack.js
+++ src/payment-api/paystack.js
@@ -3,14 +3,6 @@ import emailjs from 'emailjs-com';
 
 export const handlePayment = (email) => {
 
-  // Send mail here
-  const send = (params) => {
-    var templateParams = {      
-      email: email
-    };
-    emailjs.send('service_0mcfmwp', 'template_9lsiwyp', templateParams, 's0FLFeKbLc9PuH-nc')
-  }
-
 //   Live api_key
 //   const publicKey = 'pk_live_d03c757f196b860ca71587ed8f85aa14d800e81a'
   
@@ -32,7 +24,13 @@ export const handlePayment = (email) => {
       let message = 'Payment complete! Reference: ' + response.reference;
       alert(message);
       
-      send(email)
+      // Send mail here
+      var templateParams = {  
+        name: ''
+      email: email
+    };
+    emailjs.send('service_0mcfmwp', 'template_9lsiwyp', templateParams, 's0FLFeKbLc9PuH-nc')
+      
       window.location.href = "/welcome"
     }
   });
