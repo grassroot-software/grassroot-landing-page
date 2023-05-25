a5c124b4d0a553e4219045747355faf7ae4b36f5
Bright Kportiklah <littyofficials@gmail.com>
2023-05-25 07:53:04 -0400

    Update API_KEY

--- src/payment-api/paystack.js
+++ src/payment-api/paystack.js
@@ -11,7 +11,12 @@ export const handlePayment = (email) => {
     emailjs.send('service_0mcfmwp', 'template_9lsiwyp', templateParams, 's0FLFeKbLc9PuH-nc')
   }
 
-  const publicKey = 'pk_live_d03c757f196b860ca71587ed8f85aa14d800e81a'
+//   Live api_key
+//   const publicKey = 'pk_live_d03c757f196b860ca71587ed8f85aa14d800e81a'
+  
+//   Test api_Key
+  const publicKey = 'pk_test_bc4396a87abbbf22ff958db759d20fd8928760a1'
+
 
   const handler = PaystackPop.setup({
     key: publicKey, // Replace with your public key
