80c6cd4162c5b364b70ccf6b138cd854193fe878
Yaya HC <72512911+yayahc@users.noreply.github.com>
2023-05-26 00:22:04 +0000

    Update paystack.js

--- src/payment-api/paystack.js
+++ src/payment-api/paystack.js
@@ -1,19 +1,9 @@
 import PaystackPop from '@paystack/inline-js';
-import emailjs from 'emailjs-com';
+
 
 export const handlePayment = (email) => {
   
-      // Send mail here
-      var templateParams = {        
-      email: email
-    };
-      emailjs.send('service_0mcfmwp', 'template_9lsiwyp', templateParams,'s0FLFeKbLc9PuH-nc')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });  
-   
+
 
 //   Live api_key
   const publicKey = 'pk_live_d03c757f196b860ca71587ed8f85aa14d800e81a'
